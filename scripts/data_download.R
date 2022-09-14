2fc0abf153974efe3bc9e2ff22b6d87925886583
emench <emcanucks@gmail.com>
2022-09-14 14:46:54 -0700

    downloads and unzips data from dryad with system date

--- scripts/data_download.R
+++ scripts/data_download.R
@@ -2,49 +2,20 @@
 ############# Data Download ################
 ############################################
 # Emma Menchions, Sept 10/22
-# This script is to prepare the data from Thiffault et al. (2016) for another analysis
+# This script downloads the data from Thiffault et al. (2016) 
 
-install.packages("rdryad")
-library(rdryad)
+library(here)
+dir.create("data")
 
-# Create directories
-dir.create("data/")
-dir.create("scripts/")
+## downloading from dryad url ----
 
-dir.create("data/url")
-dir.create("data/dryad")
-dir.create("data/github")
+data.url <- "https://datadryad.org/stash/downloads/file_stream/41575"
+metadata.url <- "https://datadryad.org/stash/downloads/file_stream/41576"
 
-## download from a url
-# downloaded on 2022-09-10
-data.url <- 
-metadata.url <- 
+data.dest.file <- paste0("data/","Data_ThriffaultEtAl_EcoEvo_",Sys.Date(),".zip")
+metadata.dest.file <- paste0("data/","Metadata_ThriffaultEtAl_EcoEvo_",Sys.Date(),".txt")
 
-data.dest.file <- here::here("")
-metadata.dest.file <- here::here("")
+unzip(download.file(url = data.url, destfile = data.dest.file),exdir = here::here("data"))
+download.file(url= metadata.url, destfile = metadata.dest.file)
 
-download.file(url = "https://datadryad.org/stash/downloads/file_stream/41575", destfile = data.dest.file)
-download.file(url="https://datadryad.org/stash/downloads/file_stream/41576", destfile = metadata.des.file)
-
-write.csv(data.dest.file, file=paste0("name","Sys.Date()",".csv"))
-
-
-# from dryad
-
-# doi: https://doi.org/10.5061/dryad.4767v
-rdryad::dryad_get_cache()
-
-# setting custom cache location
-# https://github.com/emench/Menchions-BIOL548T/pull/36
-rdryad_cache <- dryad_get_chache()
-rdryad_cache$cache_path_set(full_path=normalizePath("here::here(data/dryad"),mustWork = F )
-# mustWork = F because path doesn't exist yet
-rdryad_cache $cache_path_get
-dryad_download(dois="doi.org/10.5061/dryad.4767v")
-
-# github
-
-dir.create(here::here())
-
-
-r
+unzip(list.files(path = here::here("data"), pattern = "*.zip", full.names = TRUE),exdir = here::here("data"))
